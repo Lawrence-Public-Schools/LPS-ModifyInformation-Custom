5aafa9a542d73051734f1820d03e0c9d6f5ef2a9
Logan Arias <71857092+Logan-Arias-LPS@users.noreply.github.com>
2026-04-16 11:30:26 -0400

    Simplify looking for the bottom of the page
    Simplify looking for the bottom of the page by looking for the "Counselors assigned" row then pick the next row under that

--- WEB_ROOT/admin/javascript/LPS-MIC-addAdditionalInformation-Table.js
+++ WEB_ROOT/admin/javascript/LPS-MIC-addAdditionalInformation-Table.js
@@ -1,11 +1,7 @@
 function AddLPSAdditionalInformationTable() {
 
     // Find the <tr> containing the "Auto-assign IDs for this student" link
-    var $autoAssignIdRow = $j('table:first tbody tr').filter(function () {
-        return $j(this).find('td[colspan="2"][align="center"] a').filter(function () {
-            return $j(this).text().trim() === 'Auto-assign IDs for this student';
-        }).length > 0;
-    });
+    var $autoAssignIdRow = $j('form div table tbody tr#modifyDataPageTRCounselor').next()
 
     // Grab all the rows from the hidden Additional Information table (except the wrapper table/tbody)
     // $additionalInfoRows stores all the rows from the hidden Additional Information table
